b88f01a633b946a116b4120d0c7a1a3966c132e6
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-09-24 07:24:31 +0700

    add route guard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import AdminLayout from "../pages/admin/AdminLayout";
 import ParkingManagementPage from "../pages/admin/ParkingMapPage";
 import MapEditorPage from "../pages/admin/MapEditorPage";
@@ -9,6 +9,7 @@ import Login from "../pages/auth/LogIn";
 import Register from "../pages/auth/Register";
 import ParkingRecordsPage from "../pages/admin/ParkingRecordsPage";
 import ManageUsersPage from "../pages/admin/ManageUsersPage";
+import { ACCESS_TOKEN_KEY_NAME } from "../appConst";
 
 
 function AppRoutes() {
@@ -17,14 +18,18 @@ function AppRoutes() {
             <Route path="/about" element={<div>About</div>} />
 
             {/* Admin */}
-            <Route path="/admin" element={<AdminLayout />}>
-                <Route index element={<ParkingManagementPage />} />
-                <Route path="records" element={<ParkingRecordsPage />} />
-                <Route path="users" element={<ManageUsersPage />} />
+            <Route element={<RequireAuth />}>
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route index element={<ParkingManagementPage />} />
+                    <Route path="records" element={<ParkingRecordsPage />} />
+                    <Route path="users" element={<ManageUsersPage />} />
+                </Route>
             </Route>
-            <Route path="/" element={<UserLayout />} >
-                <Route index element={<BookingParkingMap />} />
-                <Route path="history" element={<BookingHistory />} />
+            <Route element={<RequireAuth />}>
+                <Route path="/" element={<UserLayout />} >
+                    <Route index element={<BookingParkingMap />} />
+                    <Route path="history" element={<BookingHistory />} />
+                </Route>
             </Route>
 
             <Route path="/login" element={<Login />} />
@@ -33,6 +38,16 @@ function AppRoutes() {
             <Route path="*" element={<div>404</div>} />
         </Routes>
     );
+}
+
+function RequireAuth() {
+    const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY_NAME);
+
+    if (!accessToken) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return <Outlet />;
 }
 
 export default AppRoutes;
